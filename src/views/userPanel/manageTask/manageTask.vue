445dca5adda970dc59fdeac3a5781afa1472958f
Rana73 <71001363+Rana73@users.noreply.github.com>
2023-03-10 12:13:11 +0600

    Update manageTask.vue

--- src/views/userPanel/manageTask/manageTask.vue
+++ src/views/userPanel/manageTask/manageTask.vue
@@ -8,7 +8,7 @@
             <div class="text-center title">Pending</div>
             <div class="drag-card">
                 <draggable class="list-group" tag="ul" v-model="pending" v-bind="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false" @change="updateTaskList">
-                    <transition-group type="transition" :name="'flip-list'">
+                    <transition-group name="no" class="list-group" tag="ul">
                     <li class="list-group-item" v-for="element in pending" :key="element.id">
                         <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
                         <div class="item-design"> <b class="title-design">{{element.name.slice(0, 15)}}</b><br>
@@ -27,8 +27,8 @@
           <div class="mx-2 my-2">
             <div class="text-center title">In Progress</div>
             <div class="drag-card">
-                <draggable class="list-group" tag="ul" v-model="progress" v-bind="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false" @change="updateTaskList">
-                    <transition-group type="transition" :name="'flip-list'">
+                <draggable class="list-group" tag="ul" v-model="progress" v-bind="dragOptions" :move="onMove" @change="updateTaskList">
+                    <transition-group name="no" class="list-group" tag="ul">
                     <li class="list-group-item" v-for="element in progress" :key="element.id">
                         <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
                         <div class="item-design"> <b class="title-design">{{element.name.slice(0, 15)}}</b><br>
@@ -46,8 +46,8 @@
           <div class="mx-2 my-2">
             <div class="text-center title">Testing</div>
             <div class="drag-card">
-                <draggable class="list-group" tag="ul" v-model="testing" v-bind="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false" @change="updateTaskList">
-                    <transition-group type="transition" :name="'flip-list'">
+                <draggable class="list-group" tag="ul" v-model="testing" v-bind="dragOptions" :move="onMove" @change="updateTaskList">
+                    <transition-group name="no" class="list-group" tag="ul">
                     <li class="list-group-item" v-for="element in testing" :key="element.id">
                         <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
                         <div class="item-design"> <b class="title-design">{{element.name.slice(0, 15)}}</b><br>
@@ -65,8 +65,8 @@
           <div class="mx-2 my-2">
             <div class="text-center title">Done</div>
             <div class="drag-card">
-                <draggable class="list-group" tag="ul" v-model="done" v-bind="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false" @change="updateTaskList">
-                    <transition-group type="transition" :name="'flip-list'">
+                <draggable class="list-group" tag="ul" v-model="done" v-bind="dragOptions" :move="onMove" @change="updateTaskList">
+                    <transition-group name="no" class="list-group" tag="ul">
                     <li class="list-group-item" v-for="element in done" :key="element.id">
                         <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
                         <div class="item-design"> <b class="title-design">{{element.name.slice(0, 15)}}</b><br>
